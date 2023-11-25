fd0af1bcd7a3d9383153f66b52e3e82e0ae03eb6
RebinTahir <earth.rebin@gmail.com>
2023-11-25 20:37:33 +0300

    scripts need to be updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -95,9 +95,7 @@
 
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.8/js/jquery.dataTables.min.js"></script>
-        
+       
         <script>
             // create new post
             function newpost() {
